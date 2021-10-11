ecc54c3288145378e3b570385665bc095c6ac41e
akash yadav <akashyadav0112@gmail.com>
2021-10-11 18:56:18 +0530

    updated

--- src/routes/v1/person.route.js
+++ src/routes/v1/person.route.js
@@ -6,15 +6,15 @@ const personController = require('../../controllers/person.controller');
 
 const router = express.Router();
 
-router.route('/')
-.post(auth('managePersons'),validate(personValidation.createPerson), personController.createPerson)
-.get(auth('getPersons') ,validate(personValidation.getPersons), personController.getPersons);
+router
+.route('/')
+.post(auth('managePersons'), validate(personValidation.createPerson), personController.createPerson)
+.get(auth('getPersons'), validate(personValidation.getPersons), personController.getPersons);
 
 router
-  .route('/:userId')
-  .get(auth('getUsers'), validate(personValidation.getPersonById), personController.getPersonById)
-  .put( auth('managePersons'),validate(personValidation.updatePerson), personController.updatePerson)
-  .delete(auth('managePersons'),validate(personValidation.deletePerson), personController.deletePerson);
+.route('/:userId')
+.get(auth('getPersons'), validate(personValidation.getPersonById), personController.getPersonById)
+.put(auth('managePersons'), validate(personValidation.updatePerson), personController.updatePerson)
+.delete(auth('managePersons'), validate(personValidation.deletePerson), personController.deletePerson);
 
 module.exports = router;
-
